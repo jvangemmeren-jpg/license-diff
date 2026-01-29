f900992c38525854ea96b3f4c618ab280b85bb4c
jvanggemmer <jvangemmeren@yahoo.de>
2026-01-29 11:05:44 +0100

    Test-Projekte

--- src/NugetDemo/NugetDemo/obj/Debug/net8.0/NugetDemo.AssemblyInfo.cs
+++ src/NugetDemo/NugetDemo/obj/Debug/net8.0/NugetDemo.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LicenseDiffTool")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NugetDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e04a938df4d24265d7f184aa7ac884628a6c173")]
-[assembly: System.Reflection.AssemblyProductAttribute("LicenseDiffTool")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LicenseDiffTool")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacc3f603f7026cdd13f4fb2b1e74cc1b3c00e80")]
+[assembly: System.Reflection.AssemblyProductAttribute("NugetDemo")]
+[assembly: System.Reflection.AssemblyTitleAttribute("NugetDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
